dd8e0f39e1f1016ba238cf10bb899a405d2c5bac
Mike Houghton <mike_k_houghton@yahoo.co.uk>
2019-03-17 21:43:17 +0000

    Added r c to data constructor

--- src/Matrix.hs
+++ src/Matrix.hs
@@ -15,17 +15,27 @@ instance DivSupported Integer where divOp = div
 instance DivSupported Double  where divOp = (/)
 instance DivSupported Float   where divOp = (/)   
 
-
-newtype Matrix (r :: Nat) (c :: Nat) a = Matrix [[a]] deriving Show
+type R = Int
+type C = Int
+data Matrix (r :: Nat) (c :: Nat) a = Matrix R C [[a]] deriving Show
 
 instance (Num a ) => Semigroup ( Matrix (n :: Nat) (n :: Nat) a) where 
     (<>) = mul
+ 
+-- instance (Num a ) => Monoid  ( Matrix (n :: Nat) (n :: Nat) a) where
+--     mempty =  [[]] 
+    
+--     
+      
+identity :: (Num a ) => Int -> [[a]]
+identity n | n <= 0 = [[]]
+           | otherwise = take n (fmap (take n) rc) where 
+                           rc = (1 : repeat 0) : fmap (0 :) rc
+ 
 
-instance (Num a ) => Monoid  ( Matrix (n :: Nat) (n :: Nat) a) where
-    mempty = Matrix [[]]
 
 instance Functor ( Matrix (n :: Nat) (m :: Nat) ) where
-    fmap f (Matrix xs ) = Matrix ((fmap . fmap) f xs)
+    fmap f (Matrix r c xs ) = Matrix r c ((fmap . fmap) f xs)
 
 
 -- You can multiply two matrices if, and only if,
@@ -34,7 +44,7 @@ instance Functor ( Matrix (n :: Nat) (m :: Nat) ) where
 -- Otherwise, the product of two matrices is undefined.
 
 mul :: Num a => Matrix n m a -> Matrix m p a  -> Matrix n p a
-mul (Matrix xs) (Matrix ys ) = Matrix $ with xs $ \rowi -> 
+mul (Matrix r c xs) (Matrix r' c' ys ) = Matrix r c' $ with xs $ \rowi -> 
                                          with ys' $ \coli ->
                                           sum $ zipWith (*) rowi coli
                                         where ys'= L.transpose ys
@@ -43,7 +53,7 @@ with ::  [a] -> (a -> b) -> [b]
 with = flip fmap
      
 mul' :: Num a => Matrix n m a -> Matrix m p a  -> Matrix n p a
-mul' (Matrix xs) (Matrix ys ) = Matrix $ L.transpose . map (applyRow xs) . L.transpose $ ys 
+mul' (Matrix r c xs) (Matrix r' c' ys ) = Matrix r c' $ L.transpose . map (applyRow xs) . L.transpose $ ys 
   
 applyRow :: Num a => [[a]] -> [a] -> [a]
 applyRow [ys]     xs = [sum $ zipWith (*) xs ys]
@@ -55,16 +65,16 @@ scalarMul x  = fmap (* x)
 
 
 transposeM ::  Matrix n m a -> Matrix m n a 
-transposeM (Matrix xs) = Matrix (L.transpose xs)
+transposeM (Matrix r c xs) = Matrix c r (L.transpose xs)
 
 add :: Num a => Matrix n m a -> Matrix n m a  -> Matrix n m a
-add (Matrix xs) (Matrix ys ) = Matrix $ zipWith (zipWith (+)) xs  ys
+add (Matrix r c xs) (Matrix _ _   ys ) = Matrix r c $ zipWith (zipWith (+)) xs  ys
 
 sub :: Num a => Matrix n m a -> Matrix n m a  -> Matrix n m a
-sub (Matrix xs) (Matrix ys ) = Matrix $ zipWith (zipWith (-)) xs ys
+sub (Matrix r c xs) (Matrix _ _ ys ) = Matrix r c $ zipWith (zipWith (-)) xs ys
                                                                                        
 sqOp :: Num a => (a -> a -> a) -> Matrix n m a -> Matrix n m a -> Matrix n m a   
-sqOp f (Matrix xs) (Matrix ys ) = Matrix $ zipWith (zipWith f) xs ys                             
+sqOp f (Matrix r c xs) (Matrix _ _ ys ) = Matrix r c $ zipWith (zipWith f) xs ys                             
 
 add' :: Num a => Matrix n m a -> Matrix n m a  -> Matrix n m a
 add' = sqOp (+)
@@ -77,24 +87,28 @@ divEls = sqOp divOp
 
 
 mu :: Matrix 3 3 Int
-mu = Matrix [[1,2,2],[1,1,2], [4,5,6]]
+mu = Matrix 3 3 [[1,2,2],[1,1,2], [4,5,6]]
 
 mv :: Matrix 4 2 Int
-mv = Matrix   [[1,2],[3,4], [1,1], [4,4]]
+mv = Matrix 4 2  [[1,2],[3,4], [1,1], [4,4]]
 
 ma :: Matrix 5 3 Double
-ma = Matrix [[90,80,40],
-             [90,60,80],
-             [60,50,70],
-             [30,40,70],
-             [30,20,90]]
+ma = Matrix 5 3 [[90,80,40],
+                [90,60,80],
+                [60,50,70],
+                [30,40,70],
+                [30,20,90]]
 
 mOnes :: Matrix 5 5 Double
-mOnes = Matrix [[1,1,1,1,1],
-                [1,1,1,1,1],
-                [1,1,1,1,1],
-                [1,1,1,1,1],
-                [1,1,1,1,1]]
+mOnes = Matrix 5 5 [[1,1,1,1,1],
+                    [1,1,1,1,1],
+                    [1,1,1,1,1],
+                    [1,1,1,1,1],
+                    [1,1,1,1,1]]
 
 devM = ma `sub` ( 0.2 `scalarMul` (mOnes `mul` ma))
 covM = transposeM devM `mul` devM
+
+
+           
+            
